410af5c8302c9dbb33a89b78d05694a737adeb94
DanishAliUmar <danishaliumar4@gmail.com>
2024-08-10 17:31:10 +0500

    complete navbar and sidebar ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,115 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Svgs from '../Assets/svgs';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-10 py-3 bg-slate-400">
-      <div className="logo"><img src="" alt="" /></div>
-      <ul className="flex justify-center">
-        <li className="mx-4">
-          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/about-us" className="text-white hover:text-gray-300">About Us</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/our-services" className="text-white hover:text-gray-300">Our Services</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/pricing-plan" className="text-white hover:text-gray-300">Pricing</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/team-member" className="text-white hover:text-gray-300">Team</Link>
-        </li>
-      </ul>
-      <div className="support flex items-center gap-2">
-        <div className="whatsapp"><Svgs.Phone className={'w-8 h-8'}/></div>
+    <>
+      <nav className="flex items-center justify-between px-10 py-5 bg-gradient-to-b from-[#000] to-[#F2295B00]">
+        <div className="w-40 h-auto object-contain"><img src="public/Images/Logo/Logo.png" alt="" /></div>
+        <ul className="lg:flex hidden justify-center ">
+          <li className="mx-4">
+            <Link to="/" className="group hover:text-[#f8955b] text-white">Home<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+          </li>
+          <li className="mx-4">
+            <Link to="/about-us" className="group hover:text-[#f8955b] text-white">About Us<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+          </li>
+          <li className="mx-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger className='focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none'>
+                <div className="group hover:text-[#f8955b] text-white">Services<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></div></DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Our Services</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Branding</DropdownMenuItem>
+                <DropdownMenuItem>Web Development</DropdownMenuItem>
+                <DropdownMenuItem>Digital Marketing</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </li>
+          <li className="mx-4">
+            <Link to="/pricing-plan" className="group hover:text-[#f8955b] text-white">Pricing<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+          </li>
+          <li className="mx-4">
+            <Link to="/team-member" className="group hover:text-[#f8955b] text-white">Team<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+          </li>
+        </ul>
+        <div className="hidden support lg:flex items-center gap-2">
+          <div className="whatsapp"><Svgs.Phone className={'w-10 h-10 p-2 fill-white rounded-full border-white border'} /></div>
+          <div className="text-white">
+            <p className="">Support Center</p>
+            <h4 className="hover:text-[#f8955b] font-medium">+00-123-456-789</h4>
+          </div>
 
-      </div>
-    </nav>
+        </div>
+        <div className="lg:hidden block">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" className={'w-fit h-fit !bg-transparent !border-none !outline-none'}><Svgs.Hambar className={'w-5 h-auto fill-white'} /></Button>
+            </SheetTrigger>
+            <SheetContent className={'flex items-center justify-between flex-col'}>
+              <ul className="flex flex-col items-center justify-center font-bold space-y-10 flex-1">
+                <li className="mx-4">
+                  <Link to="/" className="group hover:text-[#f8955b]">Home<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+                </li>
+                <li className="mx-4">
+                  <Link to="/about-us" className="group hover:text-[#f8955b]">About Us<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+                </li>
+                <li className="mx-4">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className='focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none'>
+                      <div className="group hover:text-[#f8955b]">Services<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></div></DropdownMenuTrigger>
+                    <DropdownMenuContent className={'text-center p-4 py-2'}>
+                      <DropdownMenuLabel>Branding</DropdownMenuLabel>
+                      <DropdownMenuLabel>Web Development</DropdownMenuLabel>
+                      <DropdownMenuLabel>Digital Marketing</DropdownMenuLabel>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </li>
+                <li className="mx-4">
+                  <Link to="/pricing-plan" className="group hover:text-[#f8955b]">Pricing<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+                </li>
+                <li className="mx-4">
+                  <Link to="/team-member" className="group hover:text-[#f8955b]">Team<div class="rounded-[30px] bg-[#f8955b] h-[3px] w-[0px] group-hover:w-full transition-all duration-400"></div></Link>
+                </li>
+              </ul>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <div className="support flex items-center gap-2">
+                    <div className="whatsapp"><Svgs.Phone className={'w-10 h-10 p-2 fill-black rounded-full border-black border'} /></div>
+                    <div className="text-black">
+                      <p className="">Support Center</p>
+                      <h4 className="hover:text-[#f8955b] font-medium">+00-123-456-789</h4>
+                    </div>
+                  </div>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-red-400'>
+    <div className=''>
       working  on hme page ....
     </div>
   )
