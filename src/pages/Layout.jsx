582bf2bd08f0625f917c8d39c94c6f3e7e54af27
DanishAliUmar <danishaliumar4@gmail.com>
2024-08-13 03:33:23 +0500

    add bg lines layout ...

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-[#F8955B] !bg-center !bg-auto" style={{background: 'url(/Images/assets/bg-path.png)', backgroundAttachment:'scroll'}}>
       <Navbar />
       <div className="flex-grow">
         <Outlet />
