54e80da04e0a207ed55e5bb5c0a1ddc114212034
DanishAliUmar <danishaliumar4@gmail.com>
2024-08-29 10:34:24 +0500

    working on header layout ..

---
+++ src/components/SectionLayout/HeaderLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderLayout = () => {
+  return (
+    <div>
+      HeaderLayout
+    </div>
+  )
+}
+
+export default HeaderLayout
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Svgs from '@/Assets/svgs';
 import Contact from '/Images/Contact/Contact.jpg';
 import Contactimage2 from '/Images/Contact/Contactimage2.jpg';
+import HeaderLayout from '@/components/SectionLayout/HeaderLayout';
 
 
 
 const ContactUs = () => {
     return (
         <>
-          
+          <HeaderLayout/>
         </>
     )
 }
