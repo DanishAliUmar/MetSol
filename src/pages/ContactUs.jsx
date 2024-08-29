7d05cf0c818b6bf7dbf949f4667a9b0be47c5716
DanishAliUmar <danishaliumar4@gmail.com>
2024-08-29 23:59:50 +0500

    working on contact us page form ..

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -12,7 +12,19 @@ const ContactUs = () => {
         <>
           <HeaderLayout heading={'Contact Us'} page={'Contact Us'}/>
           <SectionLayout title={'Contact Us'} className={'bg-[#f6f6f6]'} mainHeading={'Reach Us For Instant Service'} subHeading={'Consectetur libero id faucibus nisl tincidunt eget. Sed augue lacus viverra vitae congue. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Nibh praesent tristique magna sit amet.Amet dictum sit amet justo donec.'} >
-
+            <div className="flex items-center gap-10">
+                <div className="p-12">
+                    <h2 className="">Say Hello!</h2>
+                    <p className="">Lectus sit amet est placerat in. Tempor orci eu lobortis elementum nibh.</p>
+                    <img src="" alt="" />
+                    <h4 className="">Inquiries</h4>
+                    <div className="flex items-center gap-20">
+                        <p className="">info@example.com</p>
+                        <p className="">contact-us@example.com</p>
+                        <p className="">support@example.com</p>
+                    </div>
+                </div>
+            </div>
           </SectionLayout>
         </>
     )
