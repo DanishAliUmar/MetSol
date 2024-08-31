b89aa20d85dfab972cbed7dd87d5789317acb3b0
DanishAliUmar <danishaliumar4@gmail.com>
2024-08-31 23:14:47 +0500

    working on form

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-[#f8955b] text-white hover:bg-black rounded-full !px-10 !py-6",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,27 +5,72 @@ import Contactimage2 from '/Images/Contact/Contactimage2.jpg';
 import HeaderLayout from '@/components/SectionLayout/HeaderLayout';
 import SectionLayout from '@/components/SectionLayout/SectionLayout';
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
 
 
 const ContactUs = () => {
+
+  // 1. Define your form.
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        username: "",
+    },
+});
+
+// 2. Define a submit handler.
+function onSubmit(values) {
+    // Do something with the form values.
+    console.log(values);
+}
+
+
     return (
         <>
-          <HeaderLayout heading={'Contact Us'} page={'Contact Us'}/>
-          <SectionLayout title={'Contact Us'} className={'bg-[#f6f6f6]'} mainHeading={'Reach Us For Instant Service'} subHeading={'Consectetur libero id faucibus nisl tincidunt eget. Sed augue lacus viverra vitae congue. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Nibh praesent tristique magna sit amet.Amet dictum sit amet justo donec.'} >
-            <div className="flex items-center gap-10">
-                <div className="p-12">
-                    <h2 className="">Say Hello!</h2>
-                    <p className="">Lectus sit amet est placerat in. Tempor orci eu lobortis elementum nibh.</p>
-                    <img src="" alt="" />
-                    <h4 className="">Inquiries</h4>
-                    <div className="flex items-center gap-20">
-                        <p className="">info@example.com</p>
-                        <p className="">contact-us@example.com</p>
-                        <p className="">support@example.com</p>
+            <HeaderLayout heading={'Contact Us'} page={'Contact Us'} />
+            <SectionLayout title={'Contact Us'} className={'bg-[#f6f6f6]'} mainHeading={'Reach Us For Instant Service'} subHeading={'Consectetur libero id faucibus nisl tincidunt eget. Sed augue lacus viverra vitae congue. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Nibh praesent tristique magna sit amet.Amet dictum sit amet justo donec.'} >
+                <div className="flex items-center gap-10">
+                    <div className="p-10 bg-slate-100">
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                                <FormField
+                                    control={form.control}
+                                    name="username"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Username</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="shadcn" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                This is your public display name.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form>
+                    </div>
+                    <div className="p-12">
+                        <h2 className="">Say Hello!</h2>
+                        <p className="">Lectus sit amet est placerat in. Tempor orci eu lobortis elementum nibh.</p>
+                        <img src="" alt="" />
+                        <h4 className="">Inquiries</h4>
+                        <div className="flex items-center gap-20">
+                            <p className="">info@example.com</p>
+                            <p className="">support@example.com</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-          </SectionLayout>
+            </SectionLayout>
         </>
     )
 }
