4504f7eb45ee6f819661e6b2452960436238d7a5
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-08 03:37:32 +0500

    working on othere sections in services page ..

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -9,8 +9,8 @@ const OurServices = () => {
     return (
         <div>
             <HeaderLayout heading={'Services'} page={'Services'} />
-            <section className="grid lg:grid-cols-5 gap-10 px-5 md:py-28 sm:py-16 py-10 items-start">
-                <div className="grid lg:col-span-3 grid-cols-2 gap-5">
+            <section className="grid lg:grid-cols-5 gap-10 px-5 md:py-28 sm:py-16 py-10 !pb-0  items-start">
+                <div className="grid lg:col-span-3 grid-cols-2 gap-5 lg:sticky top-10">
                     <img src={serviceImages1} alt="" className="rounded-lg w-full max-h-56 object-cover object-top" />
                     <img src={serviceImages2} alt="" className="rounded-lg w-full max-h-56 object-cover object-top" />
                     <img src={serviceImages3} alt="" className="rounded-lg w-full max-h-56 object-cover object-top" />
@@ -35,6 +35,10 @@ const OurServices = () => {
                         <p className="text-slate-500">Photography Solution</p>
                     </div>
                 </div>
+            </section>
+            <section className='space-y-2 px-5 md:py-28 sm:py-16 py-10 !pt-20 '>
+                <h2 className="text-4xl font-semibold">Bibendum enim facilisis gravida.</h2>
+                <p className="text-slate-500">Lacus vestibulum sed arcu non odio euismod lacinia at. Nunc congue nisi vitae suscipit tellus mauris a diam.malesuada quis, sagittis ut dui. Maecenas et interdum ex. Nullam non magna eu nisi congue maximus ac vel justo. Nulla viverra sapien eros, ac convallis urna semper quis. Donec convallis turpis justo, a viverra nisi cursus sit amet. Proin aliquet erat dolor, non tempus tellus euismod pellentesque. Nam iaculis diam tellus. Donec tristique, erat sit amet finibus semper, risus ipsum volutpat lacus, sit amet ultrices augue elit sit amet velit. In facilisis vulputate vestibulum. Suspendisse potenti. Curabitur a sem purus. Ut ut elit sed leo euismod laoreet sit amet feugiat ex. Nulla et enim velit. Maecenas posuere sem quis nunc faucibus, quis rutrum nunc commodo.</p>
             </section>
         </div>
     )
