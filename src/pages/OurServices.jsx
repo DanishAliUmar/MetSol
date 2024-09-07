239393803c52217212ddc1ae9891fa14f35742d3
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-08 02:59:09 +0500

    add images ..

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -1,12 +1,22 @@
 import HeaderLayout from '@/components/SectionLayout/HeaderLayout'
 import React from 'react'
+import serviceImages1 from '/Images/assets/Service-1.jpg'
+import serviceImages2 from '/Images/assets/Service-2.jpg'
+import serviceImages3 from '/Images/assets/Service-3.jpg'
+import serviceImages4 from '/Images/assets/Service-4.jpg'
 
 const OurServices = () => {
     return (
         <div>
             <HeaderLayout heading={'Services'} page={'Services'} />
-            <section className="flex items-center">
-
+            <section className="flex gap-10">
+                <div className="flex-3">
+                    <img src="" alt="" className="" />
+                    <img src="" alt="" className="" />
+                    <img src="" alt="" className="" />
+                    <img src="" alt="" className="" />
+                </div>
+                <div className="flex-2"></div>
             </section>
         </div>
     )
