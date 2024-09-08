a9820a2fcf59743ad4c01040b361f58e5a4024b7
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-09 01:31:41 +0500

    Implement static data of services..

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -1,118 +1,12 @@
 import React, { useState } from 'react'
 import Svgs from '@/Assets/svgs';
 import HeaderLayout from '@/components/SectionLayout/HeaderLayout'
+import { Link } from 'react-router-dom';
 
-const OurServices = () => {
+const OurServices = ({servicesData}) => {
     const [activeFilter, setActiveFilter] = useState('All');
     const filters = ['All', 'Web Development', 'App Development', 'Social Media Marketing'];
 
-    const servicesData = [
-        {
-            id: 1,
-            title: 'Web Development Service',
-            category: 'Web Development',
-            highted: true,
-            image: '/Images/assets/blog-detail-01.jpg',
-            link: 'https://example.com/service1',
-        },
-        {
-            id: 2,
-            title: 'App Development Service',
-            category: 'App Development',
-            image: '/Images/assets/blog-detail-02.jpg',
-            link: 'https://example.com/service2',
-        },
-        {
-            id: 3,
-            title: 'Social Media Marketing Service',
-            highted: true,
-            category: 'Social Media Marketing',
-            image: '/Images/assets/blog-detail-03.jpg',
-            link: 'https://example.com/service3',
-        },
-        {
-            id: 4,
-            title: 'Web Development Consultation',
-            category: 'Web Development',
-            image: '/Images/assets/blog-detail-04.jpg',
-            link: 'https://example.com/service4',
-        },
-        {
-            id: 5,
-            highted: true,
-            title: 'App Development Consultation',
-            category: 'App Development',
-            image: '/Images/assets/blog-detail-05.jpg',
-            link: 'https://example.com/service5',
-        },
-        {
-            id: 6,
-            title: 'Social Media Management',
-            category: 'Social Media Marketing',
-            image: '/Images/assets/blog-detail-06.jpg',
-            link: 'https://example.com/service6',
-        },
-        {
-            id: 7,
-            title: 'Web Development Support',
-            category: 'Web Development',
-            image: '/Images/assets/blog-detail-01.jpg',
-            link: 'https://example.com/service7',
-        },
-        {
-            id: 8,
-            title: 'App Development Support',
-            category: 'App Development',
-            highted: true,
-            image: '/Images/assets/blog-detail-01.jpg',
-            link: 'https://example.com/service8',
-        },
-        {
-            id: 9,
-            title: 'Social Media Advertising',
-            category: 'Social Media Marketing',
-            image: '/Images/assets/blog-detail-02.jpg',
-            link: 'https://example.com/service9',
-        },
-        {
-            id: 10,
-            title: 'Web Development Strategy',
-            highted: true,
-            category: 'Web Development',
-            image: '/Images/assets/blog-detail-03.jpg',
-            link: 'https://example.com/service10',
-        },
-        {
-            id: 11,
-            title: 'App Development Strategy',
-            category: 'App Development',
-            image: '/Images/assets/blog-detail-04.jpg',
-            link: 'https://example.com/service11',
-        },
-        {
-            id: 12,
-            highted: true,
-            title: 'Social Media Strategy',
-            category: 'Social Media Marketing',
-            image: '/Images/assets/blog-detail-05.jpg',
-            link: 'https://example.com/service12',
-        },
-        {
-            id: 13,
-            title: 'Web Development Training',
-            category: 'Web Development',
-            image: '/Images/assets/blog-detail-06.jpg',
-            link: 'https://example.com/service13',
-        },
-        {
-            id: 14,
-            title: 'App Development Training',
-            category: 'App Development',
-            image: '/Images/assets/blog-detail-01.jpg',
-            link: 'https://example.com/service14',
-        },
-    ];
-    
     const filteredPortfolio = servicesData.filter((item) => {
         if (activeFilter === 'All') {
             return true;
@@ -169,59 +63,65 @@ const OurServices = () => {
                     <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1  gap-3">
                         <div className="">
                             {column1.map((item) => (
-                                <div key={item.id} className="space-y-5 cursor-pointer">
+                                <Link key={item.id} to={`/our-services/${item.category}`}>
+                                    <div key={item.id} className="space-y-5 cursor-pointer">
 
-                                    <div className={`relative h-64`}>
-                                        <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
-                                        <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
-                                            <div className="flex items-center justify-center size-8">
-                                                <Svgs.ArrowUp className={'scale-50'} />
+                                        <div className={`relative h-64`}>
+                                            <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
+                                            <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
+                                                <div className="flex items-center justify-center size-8">
+                                                    <Svgs.ArrowUp className={'scale-50'} />
+                                                </div>
                                             </div>
                                         </div>
+                                        <div className="pb-4">
+                                            <p className="">{item.category}</p>
+                                            <h3 className="font-bold text-lg">{item.title}</h3>
+                                        </div>
                                     </div>
-                                    <div className="pb-4">
-                                        <p className="">{item.category}</p>
-                                        <h3 className="font-bold text-lg">{item.title}</h3>
-                                    </div>
-                                </div>
+                                </Link>
                             ))}
                         </div>
                         <div className="">
                             {column2.map((item) => (
-                                <div key={item.id} className="space-y-5 cursor-pointer">
+                                <Link key={item.id} to={`/our-services/${item.category}`}>
+                                    <div key={item.id} className="space-y-5 cursor-pointer">
 
-                                    <div className="relative h-64">
-                                        <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
-                                        <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
-                                            <div className="flex items-center justify-center size-8">
-                                                <Svgs.ArrowUp className={'scale-50'} />
+                                        <div className="relative h-64">
+                                            <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
+                                            <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
+                                                <div className="flex items-center justify-center size-8">
+                                                    <Svgs.ArrowUp className={'scale-50'} />
+                                                </div>
                                             </div>
                                         </div>
+                                        <div className="pb-4">
+                                            <p className="">{item.category}</p>
+                                            <h3 className="font-bold text-lg">{item.title}</h3>
+                                        </div>
                                     </div>
-                                    <div className="pb-4">
-                                        <p className="">{item.category}</p>
-                                        <h3 className="font-bold text-lg">{item.title}</h3>
-                                    </div>
-                                </div>
+                                </Link>
                             ))}
                         </div>
                         <div className="">
                             {column3.map((item) => (
-                                <div key={item.id} className="space-y-5 cursor-pointer">
+                                <Link key={item.id} to={`/our-services/${item.category}`}>
+                                    <div key={item.id} className="space-y-5 cursor-pointer">
 
-                                    <div className="relative h-64">
-                                        <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
-                                        <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
-                                            <div className="flex items-center justify-center size-8">
-                                                <Svgs.ArrowUp className={'scale-50'} />
+                                        <div className="relative h-64">
+                                            <img src={item.image} alt={item.title} className="w-full h-full absolute object-cover rounded-xl" />
+                                            <div className="absolute right-0 bottom-0 z-20 bg-[#F8955B] p-1 border-white border-l-8  rounded-tl-2xl border-t-8 border-solid">
+                                                <div className="flex items-center justify-center size-8">
+                                                    <Svgs.ArrowUp className={'scale-50'} />
+                                                </div>
                                             </div>
                                         </div>
+                                        <div className="pb-4">
+                                            <p className="">{item.category}</p>
+                                            <h3 className="font-bold text-lg">{item.title}</h3>
+                                        </div>
                                     </div>
-                                    <div className="pb-4">
-                                        <p className="">{item.category}</p>
-                                        <h3 className="font-bold text-lg">{item.title}</h3>
-                                    </div>
-                                </div>
+                                </Link>
                             ))}
                         </div>
                     </div>
--- src/pages/ServicesDetail.jsx
+++ src/pages/ServicesDetail.jsx
@@ -1,5 +1,6 @@
-import HeaderLayout from '@/components/SectionLayout/HeaderLayout'
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import HeaderLayout from '@/components/SectionLayout/HeaderLayout'
 import serviceImages1 from '/Images/assets/home-1-team-01.jpg'
 import serviceImages2 from '/Images/assets/slider-1-parallax-bg.jpg'
 import serviceImages3 from '/Images/assets/Service-3.jpg'
@@ -7,67 +8,72 @@ import serviceImages4 from '/Images/assets/Service-4.jpg'
 import { CheckCircle, CircleCheck, CircleCheckIcon, CircleChevronUpIcon, LucideCircleCheck } from 'lucide-react'
 import Svgs from '@/Assets/svgs'
 
-const ServicesDetail = () => {
+const ServicesDetail = ({ servicesData }) => {
+    const { serviceCategory } = useParams();
+
+    const service = servicesData.find(service => service.category.toLowerCase() === serviceCategory.toLowerCase());
+
+    if (!service) {
+        return <p>Service not found</p>;
+    }
+
+    const {
+        image,
+        description,
+        features,
+        ctaHeading,
+        ctadesc,
+        images,
+        checkpointsHeading,
+        checkpointsDesc,
+        checkpoints,
+        videoUrl,
+        videoHeading,
+        videoDesc,
+        videoDesc2,
+        videoDesc3
+    } = service;
+
+
+
     return (
         <div>
-            <HeaderLayout heading={'Services'} page={'Services'} />
+            <HeaderLayout heading={serviceCategory?.replace(/-/g, ' ')} page={'Services'} />
             <section className="grid lg:grid-cols-5 gap-10 px-5 md:py-28 sm:py-16 py-10 !pb-0  items-start">
                 <div className="grid lg:col-span-3 grid-cols-2 sm:gap-5 gap-3 lg:sticky top-10">
-                    <img src={serviceImages1} alt="" className="rounded-lg w-full sm:h-56 h-32 object-cover object-top" />
-                    <img src={serviceImages2} alt="" className="rounded-lg w-full sm:h-56 h-32 object-cover object-top" />
-                    <img src={serviceImages3} alt="" className="rounded-lg w-full sm:h-56 h-32 object-cover object-top" />
-                    <img src={serviceImages4} alt="" className="rounded-lg w-full sm:h-56 h-32 object-cover object-top" />
+                    {images.map((img, index) => (
+                        <img key={index} src={img} alt="" className="rounded-lg w-full sm:h-56 h-32 object-cover object-top" />
+                    ))}
                 </div>
                 <div className="sm:space-y-12 space-y-6 lg:col-span-2">
                     <div className="space-y-2">
-                        <p className="text-[#ffb600] font-bold uppercase">Amet nulla</p>
-                        <h2 className="text-4xl font-semibold">Risus sed vulputate</h2>
-                        <p className="text-slate-500">Odio euismod lacinia at quis. Eget nullam non nisi est sit. Praesent tristique magna sit amet purus gravida. Suspendisse faucibus interdum posuere.</p>
+                        <p className="text-[#ffb600] font-bold uppercase">Service</p>
+                        <h2 className="text-4xl font-semibold">{service.title}</h2>
+                        <p className="text-slate-500">{description}</p>
                     </div>
-                    <div className="space-y-2">
-                        <h4 className="text-xl font-semibold">Quisque non tellus  :</h4>
-                        <p className="text-slate-500">Nunc sed id semper risus in hendrerit gravida. Donec ac odio tempor orci dapibus ultrices in iaculis. Ultrices eros in cursus ut pharetra sit amet turpis.</p>
-                    </div>
-                    <div className="space-y-2">
-                        <h4 className="text-xl font-semibold">Pellentesque :</h4>
-                        <p className="text-slate-500">Ut lectus arcu bibendum at. Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam.</p>
-                    </div>
-                    <div className="space-y-2">
-                        <h4 className="text-xl font-semibold">Auctor neque :</h4>
-                        <p className="text-slate-500">Photography Solution</p>
-                    </div>
+                    {features.map((feature, index) => (
+                        <div key={index} className="space-y-2">
+                            <h4 className="text-xl font-semibold">{feature.title}</h4>
+                            <p className="text-slate-500">{feature.description}</p>
+                        </div>
+                    ))}
                 </div>
             </section>
             <section className='space-y-2 px-5 md:py-28 sm:py-16 py-10 sm:!pt-20 !pt-10 '>
-                <h2 className="sm:text-4xl text-3xl font-semibold">Bibendum enim facilisis gravida.</h2>
-                <p className="text-slate-500">Vivamus at augue eget arcu dictum. Scelerisque varius morbi enim nunc faucibus a. Libero nunc consequat Aliquam eu elit et enim cursus maximus. Donec ipsum turpis, accumsan non placerat et, convallis sed lacus. Sed in aliquam elit, id aliquam orci. Donec sed metus id justo auctor commodo ut at mi. Nunc egestas ligula nisi, ac iaculis augue iaculis maximus.</p>
-            </section>
+                <h4 className="text-xl font-semibold">{ctaHeading}</h4>
+                <p className="text-slate-500">{ctadesc}</p>  </section>
             <section className='px-5'>
                 <div className="flex  border-solid border rounded-lg">
                     <div className="flex-1 bg-slate-100 sm:p-12 p-5 rounded space-y-8">
-                        <h3 className="text-3xl font-semibold">Morbi tristique senectus</h3>
-                        <p className="text-slate-500">Amet consectetur adipiscing elit duis. Nunc id cursus metus aliquam eleifend. Enim sed faucibus turpis in eu mi bibendum neque egestas.Id consectetur purus ut faucibus pulvinar venenatis lectus magna fringilla.</p>
+                        <h3 className="text-3xl font-semibold">{checkpointsHeading}</h3>
+                        <p className="text-slate-500">{checkpointsDesc}</p>
                         <div className="flex items-center gap-10 gap-y-5 flex-wrap">
-                            <div className="flex items-center gap-2">
-                                <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
-                                <p className="text-black">Sit amet tellus cras adipiscing.</p>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
-                                <p className="text-black">Ut consequat semper viverra nam.</p>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
-                                <p className="text-black">Vulputate mi sit amet mauris commodo.</p>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
-                                <p className="text-black">Leo a diam sollicitudin tempor.</p>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
-                                <p className="text-black">Rutrum quisque non tellus.</p>
-                            </div>
+                            {checkpoints.map((checkpoint, index) => (
+                                <div className="flex items-center gap-2">
+                                    <CircleCheckIcon className='fill-[#ffb600] stroke-slate-100' />
+                                    <p className="text-black">{checkpoint}</p>
+                                </div>
+                            ))}
                         </div>
                     </div>
                     <div className="lg:px-24 p-16 m-auto hidden md:block">
@@ -76,11 +82,11 @@ const ServicesDetail = () => {
                 </div>
             </section>
             <section className='space-y-7 px-5 md:py-28 sm:py-16 py-10 sm:!pt-20 !pt-10 '>
-                <h2 className="sm:text-4xl text-3xl font-semibold text-[#ffb600]">Sodales neque sodales <span className="text-black"> ut volutpat</span></h2>
-                <p className="text-slate-500">Lacus vestibulum sed arcu non odio euismod lacinia at. Nunc congue nisi vitae suscipit tellus mauris a diam.malesuada quis, sagittis ut dui. Maecenas et interdum ex. Nullam non magna eu nisi congue maximus ac vel justo. Nulla viverra sapien eros, ac convallis urna semper quis. Donec convallis turpis justo, a viverra nisi cursus sit amet. Proin aliquet erat dolor, non tempus tellus euismod pellentesque. Nam iaculis diam tellus. Donec tristique, erat sit amet finibus semper, risus ipsum volutpat lacus, sit amet ultrices augue elit sit amet velit. In facilisis vulputate vestibulum. Suspendisse potenti. Curabitur a sem purus. Ut ut elit sed leo euismod laoreet sit amet feugiat ex. Nulla et enim velit. Maecenas posuere sem quis nunc faucibus, quis rutrum nunc commodo.</p>
-                <p className="text-slate-500">Fusce id velit ut tortor pretium viverra. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.aliquet ac metus. Nullam nec mattis diam, sit amet maximus felis. Vivamus sodales, magna quis venenatis mollis, ante dolor lobortis est, id accumsan est dui ac tortor. Mauris gravida arcu enim, ut interdum diam ornare vel. Phasellus ornare eleifend urna eu facilisis. Pellentesque ipsum nunc, pretium quis neque et, rutrum congue dui. Vivamus nec nibh id risus laoreet feugiat nec vel lacus.</p>
-                <iframe className='w-full aspect-video rounded-2xl' src={videoId} title="YT Vedio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <p className="text-slate-500">Dictum sit amet justo donec enim. Ut diam quam nulla porttitor massa id neque aliquam vestibulum.  Donec accumsan imperdiet tincidunt. Cras sapien lectus, molestie a ultricies sed, iaculis at diam. Pellentesq egestas congue ante, eu bibendum metus euismod eu. Aenean dictum mauris eros, eu porta odio pharetra pharetra. Aenean mollis mattis justo non facilisis. Fusce elementum ante sit amet posuere dapibus. Nam dapibus tincidunt metus, vitae porta nisi faucibus nec. Ut nisl lectus, luctus sed sodales.</p>
+                <h2 className="sm:text-4xl text-3xl font-semibold text-[#ffb600]">{videoHeading}</h2>
+                <p className="text-slate-500">{videoDesc}</p>
+                <p className="text-slate-500">{videoDesc2}</p>
+                <iframe className='w-full aspect-video rounded-2xl' src={videoUrl} title="YT Vedio" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p className="text-slate-500">{videoDesc3}</p>
                 <div className="flex lg:items-center lg:flex-row flex-col lg:gap-0 gap-10 justify-between sm:p-10 p-5 border border-solid rounded-2xl lg:divide-x divide-y lg:divide-y-0 divide-solid divide-[#ffb600]">
                     <div className="flex items-center gap-10 flex-1">
                         <img src={serviceImages1} className='size-28 cursor-pointer rounded-md object-cover' alt="" />
