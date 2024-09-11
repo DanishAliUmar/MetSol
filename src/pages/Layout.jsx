61786d744fc99e4c3e83daf3561f3d39c0a52e21
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-11 23:47:44 +0500

    Fix navbar Issue...

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,6 +6,7 @@ import Footer from '../components/Footer';
 const Layout = ({servicesData}) => {
   return (
     <div className="flex flex-col min-h-screen bg-[#F8955B] !bg-center !bg-auto" style={{background: 'url(/Images/assets/bg-path.png)', backgroundAttachment:'scroll'}}>
+      <Navbar servicesData={servicesData}/>
       <div className="flex-grow">
         <Outlet />
       </div>
